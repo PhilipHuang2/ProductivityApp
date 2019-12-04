b79e1763828039f59004214ce3a8903f3833bb8a
Philip Huang <philiphuang2@gmail.com>
2019-12-04 02:51:46 -0800

    Added First Button
    Scrolling Page for the first page

--- app/src/main/java/com/example/productivityapp/MainActivity.java
+++ app/src/main/java/com/example/productivityapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.productivityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.toDoListButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openToDoList();
+            }
+        });
+    }
+    public void openToDoList()
+    {
+        Intent intent = new Intent(this, ToDoList.class);
+        startActivity(intent);
     }
 }

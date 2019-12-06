e057871b53d593945fce47a4307f517f201a1e04
Philip Huang <philiphuang2@gmail.com>
2019-12-06 11:08:16 -0800

    Added Submit button for Journal Entry Activity

--- app/src/main/java/com/example/productivityapp/JournalEntryActivity.java
+++ app/src/main/java/com/example/productivityapp/JournalEntryActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class JournalEntryActivity extends AppCompatActivity {
@@ -12,6 +15,8 @@ public class JournalEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_entry);
+        Button submitButton;
+        final EditText paragraph;
 
         Intent intent = getIntent();
         TextView title;
@@ -19,5 +24,14 @@ public class JournalEntryActivity extends AppCompatActivity {
         String date = intent.getStringExtra("currentDate");
         title = findViewById(R.id.title);
         title.setText(date);
+        paragraph = findViewById(R.id.EditTextBox1);
+
+        submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paragraph.setText("");
+            }
+        });
     }
 }

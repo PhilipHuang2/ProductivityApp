198f51eee121cafc055388bf0aca690c6a1efb4a
Philip Huang <philiphuang2@gmail.com>
2019-12-05 18:56:19 -0800

    Firebase Integration
    Addes Firebase to the application,
    all the dependencies are addesd, just need to verify that the data is added.

--- app/src/main/java/com/example/productivityapp/CalenderActivity.java
+++ app/src/main/java/com/example/productivityapp/CalenderActivity.java
@@ -4,20 +4,23 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
 public class CalenderActivity extends AppCompatActivity {
 
-    CalendarView calendarView;
-    TextView myDate;
-
+    private CalendarView calendarView;
+    private TextView myDate;
+    private Button saveDataButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +37,20 @@ public class CalenderActivity extends AppCompatActivity {
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = (month + 1) + "/" + dayOfMonth +  "/" + year;
                 myDate.setText(date);
+            }
+        });
+
+        saveDataButton = findViewById(R.id.saveDate);
+        saveDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+
+                myRef.setValue("Hello, World!");
+                myDate.setText("Item has been added.");
+
             }
         });
     }

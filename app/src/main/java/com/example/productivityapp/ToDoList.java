14e95a54b9a6faf726d632e6212c77c013043478
Jona Huang <jthuang2@uci.edu>
2019-12-06 19:57:00 -0800

    Wrote method signatures

--- app/src/main/java/com/example/productivityapp/ToDoList.java
+++ app/src/main/java/com/example/productivityapp/ToDoList.java
@@ -4,14 +4,21 @@ import android.os.Bundle;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ScrollView;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class ToDoList extends AppCompatActivity {
@@ -20,6 +27,12 @@ public class ToDoList extends AppCompatActivity {
 
     private Button addTaskButton;
     private TextInputLayout addTaskInputText;
+    private ScrollView scrollView;
+
+    private ToDoItem[] toDoItems;
+
+    private ArrayList<ToDoItem> allToDoItems;
+    // private ListAdapter<To>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +41,13 @@ public class ToDoList extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance().getReference();
         addTaskButton = findViewById(R.id.addTaskButton);
+        scrollView = findViewById(R.id.scrollView);
 
-        // loadDataIntoCurrentCheckboxes();
+        toDoItems = new ToDoItem[5];
+        allToDoItems = new ArrayList<>();
+
+        loadToDoItems();
+        populateScrollView();
 
         addTaskInputText = findViewById(R.id.addTaskTextInput);
 
@@ -41,12 +59,37 @@ public class ToDoList extends AppCompatActivity {
         });
     }
 
-    private void loadDataIntoCurrentCheckboxes()
+    // Load all Firebase data into the array of 5 to do list items (in RAM).
+    private void loadToDoItems()
     {
-        // load any previous stored data into the current 5 checkboxes
+        DatabaseReference ref = database.child("ToDoListItems");
 
-        String checkBoxesText[] = new String[5];
-        // database.child("ToDoList").child("");
+        
+    }
+
+    // Dynamically instantiates checkboxes and sets their attributes (checked or unchecked
+    // and the text), max 5, in the View.
+    private void populateScrollView()
+    {
+
+    }
+
+    // Updates the array to store the previous 5 to do list items.
+    private void getPreviousFiveToDoListItems()
+    {
+
+    }
+
+    // Updates the array to store the next 5 to do list items.
+    private void getNextFiveToDoListItems()
+    {
+
+    }
+
+    // Deletes all checked to do list items
+    private void deleteCheckedToDoListItems()
+    {
+        // delete by key
     }
 
     private void onAddTextButtonClick()

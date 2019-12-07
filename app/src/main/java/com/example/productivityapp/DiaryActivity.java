91d7265709873b941952e41cc648563211c23db5
Cyan0147 <Cyan0147@hotmail.com>
2019-12-06 18:57:07 -0800

    DiaryActivity
    Diary Activity shows the list from the database

--- app/src/main/java/com/example/productivityapp/DiaryActivity.java
+++ app/src/main/java/com/example/productivityapp/DiaryActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -13,20 +16,30 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import java.text.SimpleDateFormat;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 
 public class DiaryActivity extends AppCompatActivity {
 
-
-    private DatabaseReference myRef;
+    // Google Firebase Database Reference
     private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
+    // Event Listener that listens to each child in the database
+    private ChildEventListener childEventListener;
+
+    // ArrayAdapter allows the results to be displayed in a list on the app
+    private DiaryAdapter listAdapter;
+
+    //Button references
     private Button newEntryButton, SearchButton, DeleteButton;
 
+    //Get the Current date
     SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
     String dateString = format.format( new Date()   );
 
@@ -37,6 +50,43 @@ public class DiaryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Initializes the references to the database
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Diary");
+
+        // Set up an array that will have the contents that you want to display
+        ArrayList<Diary> diaryList = new ArrayList<Diary>();
+
+        // Sets up the event listener that will specify what happens when access of a node
+        // occurs in the database
+        childEventListener = new ChildEventListener(){
+            @Override
+            // Method is run when any new node is added to the database, and once
+            // for every existing node when the activity is loaded
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                listAdapter.add( dataSnapshot.getValue(Diary.class));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+
+        // Need to add the event listener to the database
+        myRef.addChildEventListener(childEventListener);
+
+        // Sets up the list adapter to read from the results array
+        listAdapter = new DiaryAdapter(this, diaryList );
+        ListView results = findViewById(R.id.EntryList);
+        results.setAdapter(listAdapter);
 
         newEntryButton = findViewById(R.id.newEntryButton);
         newEntryButton.setOnClickListener(new View.OnClickListener() {
@@ -46,15 +96,7 @@ public class DiaryActivity extends AppCompatActivity {
             }
         });
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     public void openDiaryEntry(){

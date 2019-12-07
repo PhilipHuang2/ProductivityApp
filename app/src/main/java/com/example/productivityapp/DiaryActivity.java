989505aca27a8b5b2a8ee128d9295a7672b2d929
Cyan0147 <Cyan0147@hotmail.com>
2019-12-06 17:51:51 -0800

    DiaryActivity
    New Entry redirects to JournalEntryActivity to add a Diary entry

--- app/src/main/java/com/example/productivityapp/DiaryActivity.java
+++ app/src/main/java/com/example/productivityapp/DiaryActivity.java
@@ -1,5 +1,6 @@
 package com.example.productivityapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,6 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
 
 public class DiaryActivity extends AppCompatActivity {
 
@@ -18,12 +25,26 @@ public class DiaryActivity extends AppCompatActivity {
     private DatabaseReference myRef;
     private FirebaseDatabase database;
 
+    private Button newEntryButton, SearchButton, DeleteButton;
+
+    SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+    String dateString = format.format( new Date()   );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        newEntryButton = findViewById(R.id.newEntryButton);
+        newEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDiaryEntry();
+            }
+        });
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -35,4 +56,24 @@ public class DiaryActivity extends AppCompatActivity {
 //        });
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public void openDiaryEntry(){
+
+        String m = dateString.substring(0,2);
+        int month = Integer.parseInt(m);
+        String d = dateString.substring(3,5);
+        int day = Integer.parseInt(d);
+        String y = dateString.substring(6,10);
+        int year = Integer.parseInt(y);
+
+
+        //newEntryButton.setText(dateString);
+        Intent intent = new Intent(this, JournalEntryActivity.class);
+        intent.putExtra("month", month);
+        intent.putExtra("day", day);
+        intent.putExtra("year", year);
+        startActivity(intent);
+
+    }
+
 }
